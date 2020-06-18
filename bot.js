38ac1169c1dc0b7d63ed3cbe6765ad8f8cadd503
Glitch (test-noob) <none>
2020-06-18 04:17:10 +0000

    🎫🍜 Checkpoint ./bot.js:17787122/50

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ const client = new commando.CommandoClient({
 
 client.once("ready", () => {
   client.user.setPresence({
-    game: { name: "Touching me" },
+    game: { name: "58 help all" },
     status: "Invisible"
   });
 });
