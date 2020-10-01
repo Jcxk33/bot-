11305a4f12daa9b8eaf6f661f28052d4326272e4
Glitch (test-noob) <none>
2020-10-01 17:10:19 +0000

    👬📫 Checkpoint ./commands/miscellaneous/great.js:23136723/2679 ./commands/miscellaneous/changelog.js:23136723/12

--- commands/miscellaneous/great.js
+++ commands/miscellaneous/great.js
@@ -1,8 +1,5 @@
 const Discord = require("discord.js");
 const { Command } = require("discord.js-commando");
-const request = require("request-promise");
-const yesServers = "[746921954803581008]"
-
 module.exports = class great extends Command {
   constructor(client) {
     super(client, {
@@ -14,33 +11,18 @@ module.exports = class great extends Command {
     });
   }
   async run(msgObject, { target, reason }) {
-    if (yesServers.includes(msgObject.guild.id)) {
-    const mainserver = msgObject.client.guilds.cache.get("759420741987991583");
-   // const attachement = new Discord.MessageAttachment('https://cdn.glitch.com/58fa0569-0e07-48e0-9812-3be4aa2724da%2Fimage0%20(1).png?v=1598684232193')
-    let channel = mainserver.channels.cache.get("746921954803581008"); /// 744203756517720146"
+    const mainserver = msgObject.client.guilds.get("746921954803581008");
+    let channel = mainserver.channels.find("id", "759420741987991583");
     channel
-      .messages.fetch()
+      .fetchMessages()
       .then(messages => {
         let randomMsg = messages.random();
-      if (randomMsg === undefined) {
-        return msgObject.reply("<#759420741987991583> does not have any messages in it.")
-      };
         let made = new Date(randomMsg.createdTimestamp);
         let date = made.toDateString();
-      if (randomMsg.attachments.size == 0) {
         msgObject.reply(
-          `<#759420741987991583> - ${date}:\n\ ${randomMsg}`, 
+          `<#759420741987991583> - ${date}:\n\  ${randomMsg.content}`
         );
-      } else {
-      console.log(randomMsg)
-        msgObject.reply(
-          `<#759420741987991583> - ${date}:\n\  ${randomMsg}`, randomMsg.attachments.first()
-        );
-      }
       })
       .catch(console.error);
-    } else {
-      return msgObject.reply("This command can only be run in the main Discord Server!")
-    }
   }
 };
