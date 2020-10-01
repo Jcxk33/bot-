c9300efdfd4e7eaa62b656bd34b84952c7144f95
Glitch (test-noob) <none>
2020-10-01 12:33:39 +0000

    👲🐵 Checkpoint ./commands/mod/gban.js:23136723/492

--- commands/mod/gban.js
+++ commands/mod/gban.js
@@ -34,15 +34,10 @@ module.exports = class gban extends Command {
   hasPermission(msgObject) {
     const MainServer = msgObject.client.guilds.get("746921954803581008");
     if (msgObject.guild.id == 746921954803581008) {
-      if (msgObject.member.roles.find(role => role.name === "Mod")) {
+            if (msgObject.member.roles.find(role => role.name === "Bot Developer")) {
         return true;
+            
       } else if (msgObject.member.roles.find(role => role.name === "Admin")) {
-        return true;
-      
-            } else if (msgObject.member.roles.find(role => role.name === "Junior Moderator")) {
-        return true;
-      
-            } else if (msgObject.member.roles.find(role => role.name === "Bot Developer")) {
         return true;
       }
       return "Sorry 😣! You must be a Moderator or Admin!";
