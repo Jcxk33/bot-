37aa1551a1ef9faa26900737cfa293d39b8d8436
Glitch (test-noob) <none>
2020-10-07 09:56:05 +0000

    💰🌏 Checkpoint ./no/kms:17787122/12546 ./commands/no/kms:17787122/12546 ./commands/miscellaneous/ssu.js:17787122/1818

---
+++ commands/miscellaneous/ssu.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const { Command } = require("discord.js-commando");
+const request = require("request-promise");
+module.exports = class changelog extends Command {
+  constructor(client) {
+    super(client, {
+      name: "ssu",
+      aliases: ["startup"],
+      group: "miscellaneous",
+      memberName: "ssu",
+      description: "Posts a server startup to the #ssu channel",
+      ownerOnly: true,
+      args: [
+        {
+          type: "string",
+          prompt: "What is the Description?",
+          key: "description"
+        }
+      ]
+    });
+  }
+  async run(msgObject, { description }) {
+    let channel = this.client.guilds
+      .get("746921954803581008")
+      .channels.find("id", "746923991339630682");
+    channel.send("@mention");
+    let Embed = new Discord.RichEmbed()
+      .setColor("#e58049")
+      .setAuthor(`${msgObject.member.displayName}`)
+      .setAvatarURL(`${msgObject.member.avatarURL}`)
+      .setDescription(`no`)
+      .setTimestamp();
+    channel.send(Embed);
+  }
+};
