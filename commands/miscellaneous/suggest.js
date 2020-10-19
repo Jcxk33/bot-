fcc1ccc88d35b93cec7c5997b631480073a8aa3d
Glitch (test-noob) <none>
2020-10-19 02:37:28 +0000

    🐉🌿 Checkpoint ./commands/miscellaneous/suggest.js:23136723/7635

--- commands/miscellaneous/suggest.js
+++ commands/miscellaneous/suggest.js
@@ -1,49 +1,14 @@
-const Discord = require("discord.js");
-const { Command } = require("discord.js-commando");
-const request = require("request-promise");
-module.exports = class changelog extends Command {
-  constructor(client) {
-    super(client, {
-      name: "suggest",
-      aliases: ["sg"],
-      group: "miscellaneous",
-      memberName: "suggest",
-      description: "Suggests something to be added into the game",
-      ownerOnly: true,
-      args: [
-        {
-          type: "string",
-          prompt: "What is the Description?",
-          key: "description"
-        }
-      ]
-    });
-  }
-  hasPermission(msgObject) {
-      if (msgObject.member.roles.find(role => role.name === "Owner")) {
-        return true;
-      } else if (
-        msgObject.author == this.client.users.get("675794471065092161")
-      ) {
-        return true;
-      } else if (msgObject.member.roles.find(role => role.name == "Verified")) {
-        return true;
-      }
-    return "Sorry 😣! You must be Verified!";
-  }
-  async run(msgObject, { description }) {
-    let channel = this.client.guilds
-      .get("746921954803581008")
-      .channels.find("id", "763212903616217129");
-    let Embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setTitle("Game Suggestion")
-      .setDescription(description)
-      .setAuthor(
-        `${msgObject.member.displayName}`,
-        `${msgObject.author.avatarURL}`
-      )
-      .setTimestamp();
-      channel.send("@Developer ", Embed);
-  }
-};
+const talkedRecently = new Set();
+    if (talkedRecently.has(msg.author.id)) {
+            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+    } else {
+
+           // the user can type the command ... your command code goes here :)
+
+        // Adds the user to the set so that they can't talk for a minute
+        talkedRecently.add(msg.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(msg.author.id);
+        }, 60000);
+    }
