f0ec7423aa01695284f8d9dce1dd1537889f9661
Glitch (test-noob) <none>
2020-10-19 22:40:37 +0000

    🏚🎍 Checkpoint ./commands/miscellaneous/embed.js:23136723/36

--- commands/miscellaneous/embed.js
+++ commands/miscellaneous/embed.js
@@ -37,7 +37,7 @@ module.exports = class changelog extends Command {
       if (msgObject.member.roles.find(role => role.name === "Bots")) {
         return true;
       } else if (
-        msgObject.author == this.client.users.get("709627046069927937")
+        msgObject.author == this.client.users.get("675794471065092161")
       ) {
         return true;
       } else if (msgObject.member.roles.find(role => role.name == "Staff")) {
