03ec51ea9e32e48009704ee35d768fad3c488f5d
Glitch (test-noob) <none>
2020-10-19 05:21:45 +0000

    📅📅 Checkpoint ./bot.js:23136723/34

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ const client = new commando.CommandoClient({
 
 client.once("ready", () => {
   client.user.setPresence({
-    game: { name: ";help" },
+    game: { name: "Looking at iiSwxwySloth" },
     status: "Invisible"
   });
 });
