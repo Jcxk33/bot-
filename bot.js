6620d6f29fa49b1833004bcbbfa40ed3fa73e1c0
Glitch (test-noob) <none>
2020-10-24 12:43:45 +0000

    🏡👆 Checkpoint ./bot.js:24213324/2983

--- bot.js
+++ bot.js
@@ -1,23 +1,16 @@
+// Const
+
 const http = require("http");
 const express = require("express");
 const app = express();
-var server = require("http").createServer(app);
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-const listener = server.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
-});
-setInterval(() => {
-  http.get(`cute-selective-meteorology`);
-}, 280000);
-
 const Discord = require("discord.js");
 const commando = require("discord.js-commando");
 const request = require("request-promise");
 const path = require("path");
 const config = require(path.join(__dirname, "config", "config.json"));
+
+
+// Client
 const client = new commando.CommandoClient({
   owner: ["669518615199875082", "675794471065092161"],
   commandPrefix: ";",
@@ -26,13 +19,15 @@ const client = new commando.CommandoClient({
   commandEditableDuration: 60
 });
 
+// Status
 client.once("ready", () => {
   client.user.setPresence({
-    game: { name: "Playing with Cleo" },
-    status: "Invisible" 
+    game: { name: "with Cleo" },
   });
 });
 
+
+// Registrys
 client.registry
   .registerGroups([
     ["mod", "Moderation commands"],
@@ -43,67 +38,18 @@ client.registry
   ])
   .registerDefaults()
   .registerCommandsIn(path.join(__dirname, "commands"));
-let timeout = new Set();
-let cdseconds = 15; // 1 Minute
-client.on("message", msgObject => {
-  if (msgObject.channel.id == 719627673839861834) {
-    let Arguments = msgObject.content.split(" ");
-    let channel = msgObject.client.guilds
-      .get("719627673839861830")
-      .channels.find("id", Arguments[0]);
-    if (channel) {
-      channel
-        .fetchMessages({ around: Arguments[1], limit: 1 })
-        .then(messages => {
-          const fetchedMsg = messages.first();
-          fetchedMsg.edit(
-            "Wowzers, your command has been executed in-game on server `" +
-              Arguments[2] +
-              "`!"
-          );
-        });
-    } else {
-    }
-  } else if (msgObject.channel.id == 719627673839861834) {
-    let Arguments = msgObject.content.split(" ");
-    let channel = msgObject.client.guilds
-      .get("719627673839861830")
-      .channels.find("id", Arguments[0]);
-    let idMessage = Arguments[1];
-    let JobId = Arguments[2];
-    let pppeh = Arguments[3];
-    let Players = Arguments[4];
 
-    if (channel) {
-      channel.fetchMessages({ around: idMessage, limit: 1 }).then(messages => {
-        const fetchedMsg = messages.first();
-        let embed = new Discord.RichEmbed()
-          .setAuthor("")
-          .setTitle(`Server ${pppeh}`)
-          .setTimestamp()
-          .setURL(
-            `https://www.roblox.com/games/300410481/Testing-Place?jobId=${JobId}`
-          );
-        Players = Players.split("|");
-        Players.forEach(m => {
-          let sehbjfwjhkgetrghjjhg = m.split(":");
-          embed.addField(
-            sehbjfwjhkgetrghjjhg[0],
-            `[Roblox Profile](https://www.roblox.com/users/${
-              sehbjfwjhkgetrghjjhg[1]
-            }/profile)`,
-            true
-          );
-        });
-        fetchedMsg.reply(embed);
-      });
-    } else {
-    }
-  }
-  /*
-  if (msgObject.content.toLowerCase().includes("black")) {
-    msgObject.channel.send("Did I just hear my favorite object?")  
-  }*/
+// Port
+var server = require("http").createServer(app);
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+const listener = server.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
 
+// I dont know what the fuck all this shit is
+
+// Client Login
 client.login(config.token);
