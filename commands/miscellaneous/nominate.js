6fb6c243a426e6f0bc65068bbc18500180d5cf8c
Glitch (test-noob) <none>
2020-11-05 19:08:17 +0000

    🛣📇 Checkpoint ./commands/miscellaneous/nominate.js:23136723/224

--- commands/miscellaneous/nominate.js
+++ commands/miscellaneous/nominate.js
@@ -16,7 +16,7 @@ module.exports = class credits extends Command {
                     prompt: 'What election would you like to join?',
                     type: 'string',
                     validate: text => {
-                        if(text == "Senate" || text == "Sheriff") return true
+                        if(text == "Senate" || text == "Gubernatorial") return true
                     }
                 }
             ]
@@ -34,8 +34,8 @@ module.exports = class credits extends Command {
     }
   }
     async run(message, args) {
-        let senatecandidate = message.guild.roles.find(r => r.name === "Senate Candidate");
-        let sheriffcand = message.guild.roles.find(r => r.name === "Sheriff Elec");
+        let senatecandidate = message.guild.roles.find(r => r.name === "Gubernatorial Elections");
+        let gubernatorialelecte = message.guild.roles.find(r => r.name === "Sheriff Elec");
         var nickname
         if (message.member.nickname) {
             nickname = message.member.nickname
@@ -48,8 +48,8 @@ module.exports = class credits extends Command {
             message.reply('Successfully joined the Senate Election! 🤗')
             } else {
             if (args.option == 'Sheriff') {
-              message.member.addRole(sheriffcand)
-            message.reply('Successfully joined the Sheriff Elections! 🤗')
+              message.member.addRole(gubernatorialelecte)
+            message.reply('Successfully joined the Gubernatorial Elections 🤗')
             } else {
               message.reply('Guess there was an error? :shrug:')
             }
