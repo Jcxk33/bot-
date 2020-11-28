c2a083e282fe3a77ee5551c5a29da1b546a29859
Glitch (test-noob) <none>
2020-11-28 04:23:30 +0000

    💎⚽️ Checkpoint ./commands/administrator/promote.js:17787122/1374:23136723/45

--- commands/administrator/promote.js
+++ commands/administrator/promote.js
@@ -11,15 +11,44 @@ module.exports = class promote extends Command {
       memberName: "promote",
       group: "administrator",
       
-      
+      args: [
+        {
+          type: "string",
+          prompt: "What is their Roblox username?",
+          key: "desiredPlayer",
+          label: "username",
+        }
+      ]
     })
   }
   
   hasPermission(message){
-    
+    const validRole = "🎄 | Christmas"
+    if(message.member.roles.some(role => role.name == validRole)){
+      return true 
+    } else {
+      return "Sorry, :persevere:! You must be a Mayflower Administrator! :grimacing:"
+    }
   }
   
-  async run(message, { args }){
+  async run(message, { desiredPlayer }){
+    let robloxToken = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0YjEwMGRkOC01MDNlLTQxZDYtYTY2My00YTcwZWMxNjBlNWYiLCJzdWIiOjgwMzU5NzcyNX0.LGFFBlKm-zdXIlZ6wXRFvjdAwCU7bww1-7Rbjhl7vow"
+    
+    let groupID = 8141418
+    
+    let playerName 
+    let playerID 
+    
+    let playerRank 
+    let playerOldRank 
+    let playerNewRank 
+    
+    
+    // Detection System 
+    a
+    
+    
+      
     
   }
 }
