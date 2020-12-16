d2dd83096dc8556b70ddac80e066a2fd68a6c666
Glitch (test-noob) <none>
2020-12-16 19:35:29 +0000

    🍃😄 Checkpoint ./commands/es/pager.js:23136723/37

--- commands/es/pager.js
+++ commands/es/pager.js
@@ -71,7 +71,7 @@ module.exports = class pager extends Command {
         if (!pg || pg === null) {
           const mainserver = msgObject.client.guilds.get("706999196124840009");
           let channel = mainserver.channels.find("id", "740496274175819777");
-          channel.send("@mod").then(PM => {
+          channel.send("@here").then(PM => {
             let embed = new Discord.RichEmbed()
               .setAuthor(msgObject.member.displayName)
               .setTitle("New Pager!")
