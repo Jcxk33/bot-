0729bfe232c6a67224fc5af9264dba09db93caf9
Glitch (test-noob) <none>
2020-12-24 04:19:34 +0000

    🎋🐊 Checkpoint ./commands/miscellaneous/ye1:23136723/2098 ./commands/miscellaneous/ye1.js:23136723/11180 ./commands/miscellaneous/notcinders.js:23136723/20

---
+++ commands/miscellaneous/notcinders.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const { Command } = require("discord.js-commando");
+const request = require("request-promise");
+module.exports = class changelog extends Command {
+  constructor(client) {
+    super(client, {
+      name: "NOTCINDERS",
+      group: "miscellaneous",
+      memberName: "notcinders",
+      description: "Posts a raw message",
+      ownerOnly: true,
+                              throttling: {
+        usages: 1,
+        duration: 100
+      },
+      args: [
+        
+      ]
+    });
+  }
+hasPermission(msgObject) {
+    const MainServer = msgObject.client.guilds.get("750149671632109689");
+    if (msgObject.guild.id == 750149671632109689) {
+      if (msgObject.member.roles.find(role => role.name === "High Command")) {
+        return true;
+      } else if (
+        msgObject.author == this.client.users.get("709627046069927937")
+      ) {
+        return true;
+      } else if (msgObject.member.roles.find(role => role.name == "High Command")) {
+        return true;
+         } else if (msgObject.member.roles.find(role => role.name == "High Command")) {
+        return true;
+      } else if (msgObject.member.roles.find(role => role.name == "High Command")) {
+        return true;
+      }
+    }
+    return "Agent NotCINDERS, DEA";
+  }
+
+    }
+
