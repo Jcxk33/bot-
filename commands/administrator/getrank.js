6d63522fe3acad52987e4c63a0f92845154766b3
Glitch (shiny-playful-mine) <none>
2021-02-05 01:33:08 +0000

    🚟🎀 Checkpoint ./commands/administrator/acceptreq.js:23136723/1526 ./commands/administrator/getrank.js:23136723/9646

--- commands/administrator/getrank.js
+++ commands/administrator/getrank.js
@@ -22,7 +22,7 @@ module.exports = class getrank extends Command {
     });
   }
 
-hasPermission(msgObject) {
+  hasPermission(msgObject) {
   if(msgObject.guild.id == 790148683097571338 || msgObject.guild.id == 790148683097571338){
      if (msgObject.member.roles.find(role => role.name === "ICF")) {
         return true;
@@ -31,7 +31,7 @@ hasPermission(msgObject) {
       ) {
         return true;
       } else if (
-        msgObject.member.roles.find(role => role.name == "Moderator")
+        msgObject.member.roles.find(role => role.name == "ICF Director")
       ) {
         return true;
       } else if (
@@ -39,13 +39,15 @@ hasPermission(msgObject) {
       ) {
         return true;
       }
-      return "Sorry 😣! You must be a Moderator or Admin!";
+      return "Sorry 😣! You must be a ICF Agent Member!";
     } else {
       return (
-        "Sorry :persevere:! You must use this command in the State of Mayflower!"
+        "Sorry :persevere:! You must use this command in the ICF / Mayflower Server!"
       );
     }
   }
+
+
   async run(message, { desiredPlayer }) {
       let robloxToken = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_610E4DFDE859056F21079F9F5FD48BF586B95275DC0FAAB55DED89DD78A54AE0C652530CC641F264232330F7AFC24E6945A0DFE5147E96E8C238211B424504D25E1FA9A8BB57EDB1B6C798C26CE41CCA1BFA752865DC738CE79F711D8DC271220F60D91EF74B4ED19F710D945F6C38DACD06C59157BEE950FFCBEAE4C94C124AEB62EE6E56A53AF1DF4E30A95DB5F30961100E16155F7F8A5B21F9E18FDBFB465256A564A2E3FE8F8F72DB06A7F686D8C67779DBCDAE890BB20BB09831330A4D477450149E575429BF435875C817F3F7331D15807BE39355FA290E37D1E521672921B3F172876308F082BDDE6FC8446C8B93E881BE1570412366DEB65EE673D5018185FDE7FE21A8D1FB8EB00126F5B6754B056F091D4EF2B369783C36C58441884939CC"
     let sentMessage = await message.reply(
@@ -64,12 +66,11 @@ hasPermission(msgObject) {
     log.setTitle(`Command Logging`);
     log.setColor(`1D37D9`);
     log.setDescription(`${message.author} has ran getrank on **${desiredPlayer}**!`);
-  log.setFooter(
+    log.setFooter(
       `Mayflower Command Logging`,
-      `https://cdn.discordapp.com/icons/800898562786590771/37333243b8096739df4b9a019f48e79b.jpg`
+      `https://cdn.discordapp.com/icons/706999196124840009/a7ab98d9916c3482d5c4f0156b786b60.png?size=128`
     );
-    log.setTimestamp(); 
-      
+    log.setTimestamp();
     this.client.channels.get(`807012671983583303`).send(log);
     
     try {
