a2a7ad64c513bf837ec75b08a7ae882454fab81e
Glitch (real-krezzo) <none>
2021-02-17 00:59:22 +0000

    😹🌓 Checkpoint ./commands/miscellaneous/serverinfo.js:23136723/67:23636748/10847 ./commands/miscellaneous/pp.js:23636748/1155 ./commands/miscellaneous/ass.js:23636748/1406

---
+++ commands/miscellaneous/ass.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const { Command } = require("discord.js-commando");;
+const { MersenneTwister19937, integer } = require('random-js');
+const texts = require('../../assets/json/butt');
+
+module.exports = class ButtCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'butt',
+			aliases: ['butts', 'ass', 'booty'],
+			group: 'fun',
+			memberName: 'butt',
+			description: 'Determines a user\'s butt quality.',
+			credit: [
+				{
+					name: 'iCrawl',
+					url: 'https://github.com/iCrawl',
+					reason: 'Code, Concept',
+					reasonURL: 'https://github.com/iCrawl/Tohru/blob/master/src/commands/fun/butts.js'
+				}
+			],
+			args: [
+				{
+					key: 'user',
+					prompt: 'Which user do you want to determine the butt quality of?',
+					type: 'user',
+					default: msg => msg.author
+				}
+			]
+		});
+	}
+
+	run(msg, { user }) {
+		const authorUser = user.id === msg.author.id;
+		if (user.id === this.client.user.id) return msg.reply('Me? I think I have the best butt around!');
+		if (this.client.isOwner(user)) {
+			if (authorUser) return msg.reply('ur butt is godly, master');
+			return msg.reply(`${user.username}'s butt is... Something, I'll say that much.`);
+		}
+		const random = MersenneTwister19937.seed(user.id);
+		const quality = integer(0, texts.length - 1)(random);
+		return msg.reply(`${authorUser ? 'ur' : `${user.username}'s`} butt is ${texts[quality]}`);
+	}
+};
