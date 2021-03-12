1fd607e16af9a01d23a89af68e391ccb3ad0e0ed
Glitch (real-krezzo) <none>
2021-03-12 02:21:55 +0000

    🐗💋 Checkpoint ./bot.js:23136723/22

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ const client = new commando.CommandoClient({
 const Constants = require('discord.js/src/util/Constants.js')
 Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`
 client.on("ready",() => {
-client.user.setActivity('with London', { type: 1, browser: "DISCORD IOS"})
+client.user.setActivity('with London', { type: 2, browser: "DISCORD IOS"})
   .then(console.log)
   .catch(console.error);
   console.log("logged in")
