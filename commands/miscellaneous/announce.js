83e965af7d93c3d7d39956efe8e3a68f369a56f0
Glitch (real-krezzo) <none>
2021-03-27 23:53:50 +0000

    💍🍁 Checkpoint ./commands/es/pager.js:23136723/964 ./commands/es/dpager.js:23136723/2185 ./commands/miscellaneous/announce.js:23136723/2

--- commands/miscellaneous/announce.js
+++ commands/miscellaneous/announce.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+  const Discord = require("discord.js");
 const { Command } = require("discord.js-commando");
 const request = require("request-promise");
 module.exports = class changelog extends Command {
