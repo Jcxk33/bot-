c0cd019ca8bd8c149d91b67fb2e3d6af18b2b1ba
Glitch (mainbot10) <none>
2021-05-03 20:29:41 +0000

    🏅🍙 Checkpoint ./commands/mod/gserver.js:23136723/80

--- commands/mod/gserver.js
+++ commands/mod/gserver.js
@@ -47,7 +47,7 @@ module.exports = class getservercommand extends Command {
   }
   async run(msgObject, { serverUser }) {
     let data = await request({
-      uri: `https://games.roblox.com/v1/games/6474583313/servers/Public?sortOrder=Asc&limit=100`,
+      uri: `https://games.roblox.com/v1/games/6735534216/servers/Public?sortOrder=Asc&limit=100`,
       json: true,
       simple: false
     }).catch(err => {
@@ -63,7 +63,7 @@ module.exports = class getservercommand extends Command {
         Servers = Servers + 1;
         embed.addField(
           `Server ${Data.playing}/${Data.maxPlayers} ${Data.id}`,
-          `[State of Mayflower](https://www.roblox.com/games/6474583313/State-of-Mayflower#)\n[Detailed Link](https://games.roblox.com/v1/games/6474583313/servers/Public?sortOrder=Asc&limit=100jobId=${Data.id})`
+          `[State of Mayflower](https://www.roblox.com/games/6735534216/State-of-Mayflower#)\n[Detailed Link](https://games.roblox.com/v1/games/6735534216/servers/Public?sortOrder=Asc&limit=100jobId=${Data.id})`
         );
       });
       embed.setDescription(`There are currently ${Servers} servers.`);
@@ -95,7 +95,7 @@ module.exports = class getservercommand extends Command {
           msgObject.reply("Sorry :angry:! This server does not exist!");
         } else {
           let data = await request({
-            uri: `https://games.roblox.com/v1/games/6474583313/servers/Public?sortOrder=Asc&limit=100`,
+            uri: `https://games.roblox.com/v1/games/6735534216/servers/Public?sortOrder=Asc&limit=100`,
             json: true,
             simple: false
           }).catch(err => {
