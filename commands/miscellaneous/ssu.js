1abd627f8fe53314c11eede3be585bbcafe7c6dd
Glitch (mainbot10) <none>
2021-05-06 02:46:58 +0000

    👯🐈 Checkpoint ./commands/miscellaneous/ssu.js:23136723/103 ./bot.js:23136723/38

--- commands/miscellaneous/ssu.js
+++ commands/miscellaneous/ssu.js
@@ -24,8 +24,8 @@ module.exports = class changelog extends Command {
     });  
   }
 hasPermission(msgObject) {
-  if(msgObject.guild.id == 808115419842871346 || msgObject.guild.id == 808115419842871346){
-     if (msgObject.member.roles.find(role => role.name === "Developer")) {
+  if(msgObject.guild.id == 832707053243727912 || msgObject.guild.id == 832707053243727912){
+     if (msgObject.member.roles.find(role => role.name === "Senior Admin")) {
         return true;
       } else if (
         msgObject.author == this.client.users.get("675794471065092161")
