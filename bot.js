475b5c3f9243dde1fd234d5c5c9130496a6f5cf1
Glitch (mainbot10) <none>
2021-08-04 21:04:26 +0000

    🎥🐟 Checkpoint ./bot.js:18951434/8554 ./commands/mod/gban.js:18951434/3946

--- bot.js
+++ bot.js
@@ -5,15 +5,15 @@ const express = require("express");
 const app = express();
 const Discord = require("discord.js");
 const commando = require("discord.js-commando");
-const request = require("request-promise");``
+const request = require("request-promise");
+``;
 const path = require("path");
 const config = require(path.join(__dirname, "config", "config.json"));
-const oneLine = require('common-tags').oneLine;
-
+const oneLine = require("common-tags").oneLine;
 
 // Client
 const client = new commando.CommandoClient({
-  owner: ["675794471065092161", "509015814079381504"],
+  owner: ["870821970994921514", "651579124850360342"],
   commandPrefix: ";",
   unknownCommandResponse: false,
   selfbot: false,
@@ -21,101 +21,120 @@ const client = new commando.CommandoClient({
 });
 
 // Status
-const Constants = require('discord.js/src/util/Constants.js')
-Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`
-client.on("ready",() => {
-client.user.setActivity('with IC', { type: 1, browser: "DISCORD IOS"})
-  .then(console.log)
-  .catch(console.error);
-  console.log("logged in")
+const Constants = require("discord.js/src/util/Constants.js");
+Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`;
+client.on("ready", () => {
+  client.user
+    .setActivity("with IC", { type: 1, browser: "DISCORD IOS" })
+    .then(console.log)
+    .catch(console.error);
+  console.log("logged in");
+});
 
-  });
-
-
-
-app.get('/', async (request, response) => {
-     response.sendStatus(200);
+app.get("/", async (request, response) => {
+  response.sendStatus(200);
 });
 
 app.get(`/get-request`, async (request, response) => {
-    response.status(200).send(client.request);
+  response.status(200).send(client.request);
 });
 
 app.post(`/verify-request`, async (request, response) => {
-    let commandRequest = client.request;
-    if(commandRequest === "No request") return response.sendStatus(200);
-    let successStatus = request.headers.success;
-    let message = request.headers.message;
+  let commandRequest = client.request;
+  if (commandRequest === "No request") return response.sendStatus(200);
+  let successStatus = request.headers.success;
+  let message = request.headers.message;
 
-    let channel = client.channels.cache.get(commandRequest.channelID);
-    if(!channel) {
-        return response.sendStatus(200);
-    }
-
-    if(successStatus == "true") {
-        if("moderator" in request.headers) {
-            channel.send(`<@${commandRequest.authorID}>`);
-            let embed = client.embedMaker(client.users.cache.get(commandRequest.authorID), "Success", message)
-            embed.addField("Ban Information", `**Moderator**: ${request.headers.moderator}\n**Reason**: ${request.headers.reason}`);
-            channel.send(embed);
-        } else {
-            channel.send(`<@${commandRequest.authorID}>`);
-            channel.send(client.embedMaker(client.users.cache.get(commandRequest.authorID), "Success", message));
-        }
-    } else {
-        channel.send(`<@${commandRequest.authorID}>`);
-        channel.send(client.embedMaker(client.users.cache.get(commandRequest.authorID), "Failure", message));
-    }
-
-    client.request = "No request";
-
+  let channel = client.channels.cache.get(commandRequest.channelID);
+  if (!channel) {
     return response.sendStatus(200);
+  }
+
+  if (successStatus == "true") {
+    if ("moderator" in request.headers) {
+      channel.send(`<@${commandRequest.authorID}>`);
+      let embed = client.embedMaker(
+        client.users.cache.get(commandRequest.authorID),
+        "Success",
+        message
+      );
+      embed.addField(
+        "Ban Information",
+        `**Moderator**: ${request.headers.moderator}\n**Reason**: ${request.headers.reason}`
+      );
+      channel.send(embed);
+    } else {
+      channel.send(`<@${commandRequest.authorID}>`);
+      channel.send(
+        client.embedMaker(
+          client.users.cache.get(commandRequest.authorID),
+          "Success",
+          message
+        )
+      );
+    }
+  } else {
+    channel.send(`<@${commandRequest.authorID}>`);
+    channel.send(
+      client.embedMaker(
+        client.users.cache.get(commandRequest.authorID),
+        "Failure",
+        message
+      )
+    );
+  }
+
+  client.request = "No request";
+
+  return response.sendStatus(200);
 });
-
-
-
 
 // CONSOLE LOGGERS FOR ANY ERRORS ETC
 client
-	.on('error', console.error)
-	.on('warn', console.warn)
-	.on('debug', console.log)
-	.on('ready', () => {
-		console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
-	})
-	.on('disconnect', () => { console.warn('Disconnected!'); })
-	.on('reconnecting', () => { console.warn('Reconnecting...'); })
-	.on('commandError', (cmd, err) => {
-		if(err instanceof commando.FriendlyError) return;
-		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
-	})
-	.on('commandBlocked', (msg, reason) => {
-		console.log(oneLine`
-			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+  .on("error", console.error)
+  .on("warn", console.warn)
+  .on("debug", console.log)
+  .on("ready", () => {
+    console.log(
+      `Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`
+    );
+  })
+  .on("disconnect", () => {
+    console.warn("Disconnected!");
+  })
+  .on("reconnecting", () => {
+    console.warn("Reconnecting...");
+  })
+  .on("commandError", (cmd, err) => {
+    if (err instanceof commando.FriendlyError) return;
+    console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+  })
+  .on("commandBlocked", (msg, reason) => {
+    console.log(oneLine`
+			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""}
 			blocked; ${reason}
 		`);
-	})
-	.on('commandPrefixChange', (guild, prefix) => {
-		console.log(oneLine`
-			Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'the default'}`}
-			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+  })
+  .on("commandPrefixChange", (guild, prefix) => {
+    console.log(oneLine`
+			Prefix ${prefix === "" ? "removed" : `changed to ${prefix || "the default"}`}
+			${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
 		`);
-	})
-	.on('commandStatusChange', (guild, command, enabled) => {
-		console.log(oneLine`
+  })
+  .on("commandStatusChange", (guild, command, enabled) => {
+    console.log(oneLine`
 			Command ${command.groupID}:${command.memberName}
-			${enabled ? 'enabled' : 'disabled'}
-			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+			${enabled ? "enabled" : "disabled"}
+			${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
 		`);
-	})
-	.on('groupStatusChange', (guild, group, enabled) => {
-		console.log(oneLine`
+  })
+  .on("groupStatusChange", (guild, group, enabled) => {
+    console.log(oneLine`
 			Group ${group.id}
-			${enabled ? 'enabled' : 'disabled'}
-			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+			${enabled ? "enabled" : "disabled"}
+			${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
 		`);
-	});
-
+  });
 
 // Registrys
 client.registry
@@ -127,7 +146,6 @@ client.registry
   ])
   .registerDefaults()
   .registerCommandsIn(path.join(__dirname, "commands"));
-
 
 // Client Login
 client.login(config.token);
--- commands/mod/gban.js
+++ commands/mod/gban.js
@@ -1,1 +1,119 @@
-//no
+const Discord = require("discord.js");
+const { Command } = require("discord.js-commando");
+const Trello = require("trello");
+const request = require("request-promise");
+var trello = new Trello(
+  "e752622bcd4de22bbc07b6838a6bba74",
+  "48d0b091506d386846fada4d7c2c285c5beccb780e16f51a582c0e2b430329c9"
+);
+
+module.exports = class gban extends Command {
+  constructor(client) {
+    super(client, {
+      name: "gban",
+      group: "mod",
+      memberName: "gban",
+      description: "Bans a user from the game",
+      guildOnly: true,
+      args: [
+        {
+          type: "string",
+          label: "username",
+          prompt: "What is the Roblox username?",
+          key: "username"
+        },
+        {
+          type: "string",
+          prompt: "What is the reason for moderating this user?",
+          key: "reason"
+        }
+      ]
+    });
+  }
+     hasPermission(msgObject) {
+  if(msgObject.guild.id == 871166263945216040 || msgObject.guild.id == 871166263945216040){
+     if (msgObject.member.roles.find(role => role.name === "Developer")) {
+        return true;
+      } else if (
+        msgObject.author == this.client.users.get("870821970994921514")
+      ) {
+        return true;
+      } else if (
+        msgObject.member.roles.find(role => role.name == "Game Moderator")
+      ) {
+        return true;
+      } else if (
+        msgObject.member.roles.find(role => role.name == "Admin")
+      ) {
+        return true;
+      }
+      return "Sorry 😣! You must be a Moderator or Admin!";
+    } else {
+      return (
+        "Sorry :persevere:! You must use this command in the State of Rosewood!"
+      );
+    }
+  }
+  async run(msgObject, { username, reason }) {
+    const editMessage = await msgObject.reply(
+      `Coolio!! Let's get on with this and ban \`${username}\``
+    );
+    let data = await request({
+      uri: `https://api.roblox.com/users/get-by-username?username=${username}`,
+      json: true,
+      simple: false
+    });
+    if (data.errorMessage) {
+      return editMessage.edit(
+        "Sorry 😣! You haven't entered a valid Roblox username!"
+      );
+    } else {
+      editMessage.edit(`Ooooh, we've got their UserID as \`${data.Id}\`!`);
+      let authorData = await request({
+        uri: `https://verify.eryn.io/api/user/${msgObject.author.id}`,
+        json: true,
+        simple: false
+      });
+      editMessage.edit(
+        `Ooooh, we've also got your UserID as \`${authorData.robloxId}\`!`
+      );
+      const webhook = new Discord.WebhookClient(
+        "872585199811186688",
+        "226QYK7ferbWaeAkVQvgMODX19ZnIjiXkzziE1xe1h6EiLjlDJtPFbfypCLTJ6Dg0BVD"
+      );
+      webhook.send("", {
+        embeds: [
+          {
+            author: {
+              name: `${authorData.robloxUsername}`
+            },
+            title: `Banned ${data.Username}`,
+            description: `"${reason}"`
+          }
+        ]
+      });
+
+      editMessage.edit(
+        `Congrats 🙌! Your command will be executed in-game shortly!`
+      );
+      editMessage.edit("Insane, Your command has been executed in-game!");
+  const log = new Discord.RichEmbed();
+    log.setTitle(`Command Logging`);
+    log.setColor(`1D37D9`);
+    log.setDescription(`${msgObject.member} has Gamebanned **${username}** for **${reason}**! `);
+     log.setFooter(
+      `Mayflower Command Logging`,
+      `https://cdn.discordapp.com/emojis/778159140219912212.png?v=1`
+    );
+    log.setTimestamp(); 
+      
+    this.client.channels.get(`820208777072082945`).send(log);
+trello.addCard(
+        `${username}:${data.Id}`,
+        `Moderator: ${authorData.robloxUsername}\nReason: ${reason}`,
+        "6053c89df3a0e81e60fefc21"
+  
+      );
+    }
+  }
+};
