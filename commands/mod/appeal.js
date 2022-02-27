c2f450d9f66aee21894afc5a0fa9e3198713d42a
Glitch (verdant-amenable-raven) <none>
2022-02-27 18:41:29 +0000

    🐆🔨 Checkpoint ./commands/mod/appeal.js:32147226/590 ./package.json:32147226/1354 ./config/config.json:32147226/118

--- commands/mod/appeal.js
+++ commands/mod/appeal.js
@@ -28,11 +28,11 @@ module.exports = class credits extends Command {
     }
 
         hasPermission(msgObject) {
-  if(msgObject.guild.id == 930029180467421215 || msgObject.guild.id == 930029180467421215){
+  if(msgObject.guild.id == 933442884311601223 || msgObject.guild.id == 933442884311601223){
      if (msgObject.member.roles.find(role => role.name === "Developer")) {
         return true;
       } else if (
-        msgObject.author == this.client.users.get("454046618589593620")
+        msgObject.author == this.client.users.get("933442884311601223")
       ) {
         return true;
       } else if (
@@ -52,7 +52,7 @@ module.exports = class credits extends Command {
     }
   }
     async run(message, args) {
-        var webhook = new Discord.WebhookClient('930041343428943872', 'ZX85EVH2Cxeemqz1YSPgaWlkeHkLo5GBTd4hx3z1z71NwcJdGWsf9Yhsp7p_XaYWIZLp')
+        var webhook = new Discord.WebhookClient('947562270433640469', 'uPBOIcZisvfiSrAIleoXIVQrDbZ1daeYbZ8VzeD3WD7VL0JXSn4zffqNaC9qPa7XIqh8')
         var nickname
         if (message.member.nickname) {
             nickname = message.member.nickname
